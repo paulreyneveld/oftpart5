3ae418dce9e40202552201128c3e564142229225
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-11-09 18:18:43 -0800

    Added functionality through 5.1

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,12 +16,23 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
-  }
 
-  const user = null
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+  
   if (user === null) {
     return (
       <div>
@@ -52,6 +65,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} is logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
