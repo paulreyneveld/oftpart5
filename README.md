086f3502bb3963c61473af4a3c42b87892560a38
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-11-11 18:03:34 -0800

    Updated with functionality through 5.12

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This repo contains answers for the OFS exercises 5.1 - 5.3, 5.5, 5.6, and 5.11.
+This repo contains answers for the OFS exercises 5.1 - 5.3, 5.5, 5.6, 5.11 and 5.12.
--- bloglist-frontend/src/components/Togglable.js
+++ bloglist-frontend/src/components/Togglable.js
@@ -33,5 +33,7 @@ const Togglable = React.forwardRef((props, ref) => {
   Togglable.propTypes = {
     buttonLabel: PropTypes.string.isRequired
   }
+
+  Togglable.displayName = "Togglable"
   
   export default Togglable
