13f461c7a96a6a8a177efcd91c0b50e61c38fd2a
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-11-17 18:31:32 -0800

    Added some thoughts to exercise 5.22

--- bloglist-frontend/cypress/integration/examples/blog_app.spec.js
+++ bloglist-frontend/cypress/integration/examples/blog_app.spec.js
@@ -74,4 +74,35 @@ describe('Blog app', function() {
     })
   })
 
+  describe.only('When logged in', function() {
+
+    beforeEach(function() {
+      cy.get('#username').type('paolov')
+      cy.get('#password').type('test')
+      cy.get('#login-button').click()
+    })
+
+    it('sorted by likes', function() {
+      cy.get('#show').click()
+      cy.get('#title').type('new blog')
+      cy.get('#author').type('Paolo')
+      cy.get('#url').type('test.com')
+      cy.get('#submit-new-blog').click()
+      cy.get('#show').click()
+
+      cy.get('#like').click()
+
+      cy.get('#show').click()
+      cy.get('#title').type('Another new blog')
+      cy.get('#author').type('Paolo')
+      cy.get('#url').type('test.com')
+      cy.get('#submit-new-blog').click()
+      cy.get('#show').click()
+
+      cy.get('#like').click()
+      cy.get('#like').click()
+      cy.get('#like').click()
+    })
+  })
+
 })
