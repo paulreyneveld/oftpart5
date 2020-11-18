7ae7e7c5d5c8ade7a134f3f27fe5bfb04cc99a6e
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-11-17 18:20:11 -0800

    Updated with functionality for 5.9

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   const blogFormRef = useRef()
 
   const dbHook = () => { blogService.getAll().then(blogs =>
-    setBlogs(blogs)
+    setBlogs(blogs.sort((a, b) => a.likes > b.likes ? -1 : 1))
   ) }
 
   useEffect(
@@ -113,6 +113,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} is logged in</p>
+      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} updateBlogLikes={updateBlogLikes} deleteBlog={deleteBlog} />
       )}
